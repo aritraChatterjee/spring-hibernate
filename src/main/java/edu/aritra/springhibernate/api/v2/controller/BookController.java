ed4940e59f28d862a7c0392ad33c8a7b5861a944
aritraChatterjee <aritra.cnw@gmail.com>
2020-09-06 20:37:29 +0200

    fixed request mapping for v2 of api

--- src/main/java/edu/aritra/springhibernate/api/v2/controller/BookController.java
+++ src/main/java/edu/aritra/springhibernate/api/v2/controller/BookController.java
@@ -1,11 +1,13 @@
 package edu.aritra.springhibernate.api.v2.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * The version v2 of the controller make use of the action pattern
  */
-@RestController(value = "/v2")
+@RestController
+@RequestMapping("/v2")
 public class BookController {
 
 }
